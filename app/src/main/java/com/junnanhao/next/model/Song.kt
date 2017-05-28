3d596bd89d70b5322fbc431035309658ad336803
Jonas <imkoche@gmail.com>
2017-05-28 13:45:49 +0800

    load music task

--- app/src/main/java/com/junnanhao/next/model/Song.kt
+++ app/src/main/java/com/junnanhao/next/model/Song.kt
@@ -10,9 +10,20 @@ open class Song(var path: String = "",
                 var duration: Int = 0,
                 var title: String = "",
                 var artist: String = "",
-                var displayName: String = "") : RealmObject() {
+                var displayName: String = "",
+                var size: Int = 0,
+                var album: String = "") : RealmObject() {
 
     override fun toString(): String {
         return "Song(path='$path', duration=$duration, title='$title', artist='$artist', displayName='$displayName')"
+    }
+
+    fun isSongValid(): Boolean {
+        return duration > MIN_DURATION && artist != UNKNOWN
+    }
+
+    companion object {
+        val MIN_DURATION: Int = 40000
+        val UNKNOWN: String = "<unknown>"
     }
 }
--- app/src/main/java/com/junnanhao/next/player/LoadMusicTask.kt
+++ app/src/main/java/com/junnanhao/next/player/LoadMusicTask.kt
@@ -5,23 +5,21 @@ import android.database.Cursor
 import android.provider.MediaStore
 import android.support.v4.content.ContentResolverCompat
 import android.support.v4.os.CancellationSignal
-import android.util.Log
 import com.junnanhao.next.model.Song
 import io.reactivex.Observable
-import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
+import io.realm.Realm
 import java.io.File
-import java.util.*
 import kotlin.collections.ArrayList
-
+import android.media.MediaMetadataRetriever
+import com.junnanhao.next.model.Song.Companion.UNKNOWN
+import android.text.TextUtils
 
 /**
  * Created by Jonas on 2017/5/27.
  * Load music task
  */
 class LoadMusicTask(private var context: Context) {
-    private val TAG = "LocalMusicPresenter"
-    private val URL_LOAD_LOCAL_MUSIC = 0
     private val MEDIA_URI = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
     private val WHERE = MediaStore.Audio.Media.IS_MUSIC + "=1 AND " + MediaStore.Audio.Media.SIZE + ">0"
     private val ORDER_BY = MediaStore.Audio.Media.DISPLAY_NAME + " ASC"
@@ -30,7 +28,7 @@ class LoadMusicTask(private var context: Context) {
     private var cancellationSignal: CancellationSignal? = null
     fun load() {
         cancellationSignal = CancellationSignal()
-        var cursor: Cursor = ContentResolverCompat.query(
+        val cursor: Cursor = ContentResolverCompat.query(
                 context.contentResolver,
                 MEDIA_URI,
                 PROJECTIONS,
@@ -39,43 +37,26 @@ class LoadMusicTask(private var context: Context) {
                 ORDER_BY, cancellationSignal)
 
         Observable.just(cursor)
-                .flatMap { cursor ->
+                .subscribeOn(Schedulers.io())
+                .observeOn(Schedulers.io())
+                .map {
+                    cursor: Cursor? ->
                     val songs: ArrayList<Song> = ArrayList()
                     if (cursor != null && cursor.count > 0) {
                         cursor.moveToFirst()
                         do {
                             val song = cursorToMusic(cursor)
-                            System.out.println(song.toString())
-                            songs.add(song)
+                            if (song.isSongValid())
+                                songs.add(song)
                         } while (cursor.moveToNext())
                     }
-                    System.out.println("size = " + songs.size)
-                    Log.d(TAG, "song size = " + songs.size)
-                    Observable.fromArray(songs)
+                    songs
                 }
-                .doOnNext {
-                    songs: List<Song>? ->
-                    Log.d(TAG, "onLoadFinished: " + songs?.size)
-                    Collections.sort(songs) { left, right -> left.displayName.compareTo(right.displayName) }
-                }
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
                 .subscribe({
-                    //                    mView!!.showProgress()
-                    t: ArrayList<Song>? ->
-                    Log.d(TAG, "onNext: size=" + t?.size)
-                }, {
-                    //                    mView!!.hideProgress()
-                    t: Throwable? ->
-                    Log.e(TAG, "onError: ", t)
-                }, {
-                    cursor.close()
-                    //                    mView!!.hideProgress()
-                }, {
-                    //                    mView!!.onLocalMusicLoaded(songs)
-//                    mView!!.emptyView(songs.isEmpty())
+                    songs: ArrayList<Song>? ->
+                    val realm: Realm = Realm.getDefaultInstance()
+                    realm.executeTransaction({ realm -> realm.copyToRealmOrUpdate(songs) })
                 })
-//        mSubscriptions.add(subscription)
     }
 
 
@@ -83,28 +64,63 @@ class LoadMusicTask(private var context: Context) {
         val realPath = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA))
         val songFile = File(realPath)
         var song: Song?
-//        if (songFile.exists()) {
-//            // Using song parsed from file to avoid encoding problems
-//            song = FileUtils.fileToMusic(songFile)
-//            if (song != null) {
-//                return song
-//            }
-//        }
-        song = Song()
-        song.title = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.TITLE))
+        if (songFile.exists()) {
+            // Using song parsed from file to avoid encoding problems
+            song = fileToMusic(songFile)
+            if (song != null) {
+                return song
+            }
+        }
 
-//        var displayName = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DISPLAY_NAME))
-//        if (displayName.endsWith(".mp3")) {
-//            displayName = displayName.substring(0, displayName.length - 4)
-//        }
-//        song!!.setDisplayName(displayName)
-//        song!!.artist(cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ARTIST)))
-//        song!!.setAlbum(cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ALBUM)))
-//        song!!.setPath(cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA)))
-//        song!!.setDuration(cursor.getInt(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DURATION)))
-//        song!!.setSize(cursor.getInt(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.SIZE)))
+        var displayName = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DISPLAY_NAME))
+        if (displayName.endsWith(".mp3")) {
+            displayName = displayName.substring(0, displayName.length - 4)
+        }
+        song = Song(displayName = displayName)
+        song.title = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.TITLE))
+        song.artist = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ARTIST))
+        song.album = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ALBUM))
+        song.path = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA))
+        song.duration = cursor.getInt(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DURATION))
+        song.size = cursor.getInt(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.SIZE))
         return song
     }
 
 
+    fun fileToMusic(file: File): Song? {
+        if (file.length() == 0L) return null
+
+        val metadataRetriever = MediaMetadataRetriever()
+        metadataRetriever.setDataSource(file.absolutePath)
+
+        val duration: Int
+
+        val keyDuration = metadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION)
+        // ensure the duration is a digit, otherwise return null song
+        if (keyDuration == null || !keyDuration.matches("\\d+".toRegex())) return null
+        duration = Integer.parseInt(keyDuration)
+
+        val title = extractMetadata(metadataRetriever, MediaMetadataRetriever.METADATA_KEY_TITLE, file.name)
+        val displayName = extractMetadata(metadataRetriever, MediaMetadataRetriever.METADATA_KEY_TITLE, file.name)
+        val artist = extractMetadata(metadataRetriever, MediaMetadataRetriever.METADATA_KEY_ARTIST, UNKNOWN)
+        val album = extractMetadata(metadataRetriever, MediaMetadataRetriever.METADATA_KEY_ALBUM, UNKNOWN)
+
+        val song = Song()
+        song.title = title
+        song.displayName = displayName
+        song.artist = artist
+        song.path = file.absolutePath
+        song.album = album
+        song.duration = duration
+        song.size = file.length().toInt()
+        return song
+    }
+
+    private fun extractMetadata(retriever: MediaMetadataRetriever, key: Int, defaultValue: String): String {
+        var value = retriever.extractMetadata(key)
+        if (TextUtils.isEmpty(value)) {
+            value = defaultValue
+        }
+        return value
+    }
 }
